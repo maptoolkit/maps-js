350e2ec7d1f38d3a62bf29fd386aedabc3f89431
db9795 <daniel@toursprung.com>
2025-01-30 18:48:31 +0100

    Formatting

--- src/js/core/utils.ts
+++ src/js/core/utils.ts
@@ -11,6 +11,9 @@ export async function get32ByteChecksum(str: string): Promise<string> {
   const data = encoder.encode(str);
   const hashBuffer = await crypto.subtle.digest("SHA-256", data);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join("").slice(0, 32);
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("")
+    .slice(0, 32);
   return hashHex;
 }
