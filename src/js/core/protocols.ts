e4b31501e729d2cd45cfa25678b7278651c33214
db9795 <daniel@toursprung.com>
2025-02-03 15:21:30 +0100

    Change DataConnector protocol handling to new api calls (simpler)

--- src/js/core/protocols.ts
+++ src/js/core/protocols.ts
@@ -1,17 +1,11 @@
 import { addProtocol as maplibreAddProtocol } from "maplibre-gl";
-import { get32ByteChecksum } from "./utils";
 import { config } from "./config";
-
-type DataConnectorRequestParams = {
-  query?: string;
-  ids?: string;
-};
 
 maplibreAddProtocol("maptoolkit", (params, abortController) => {
   return new Promise((resolve) => {
     const maptoolkitUrl = new URL(params.url);
     const [service, account, name] = maptoolkitUrl.pathname.slice(2).split("/");
-    let requestUrl;
+    let requestUrl: URL | null = null;
     if (service === "style") {
       requestUrl = new URL(`/${account}/${name}.json`, config.stylesHost);
     } else if (service === "sprite") {
@@ -19,56 +13,10 @@ maplibreAddProtocol("maptoolkit", (params, abortController) => {
       const format = params.type === "json" ? "json" : "png";
       requestUrl = new URL(`/${account}${ratio}.${format}`, config.iconsHost);
     } else if (service === "dataconnector") {
-      const ids = maptoolkitUrl.searchParams.get("ids");
-      const query = maptoolkitUrl.searchParams.get("query");
-      const url = new URL(config.dataconnectorHost);
-      url.searchParams.set("api_key", config.apiKey);
-      if (ids) {
-        get32ByteChecksum(ids).then((cacheId) => {
-          url.pathname = `/${account}/${name}/querycache`;
-          fetch(url.toString(), {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ query: cacheId, ids: JSON.stringify(ids.split(",")) }),
-          })
-            .then((response) => {
-              if (response.status !== 200) {
-                abortController.abort();
-              } else {
-                url.pathname = `/${account}/${name}.json`;
-                url.searchParams.set("query", cacheId);
-                fetch(url)
-                  .then((r) => r.json())
-                  .then((data) => resolve({ data }));
-              }
-            })
-            .catch(() => abortController.abort());
-        });
-      } else if (query) {
-        url.pathname = `/${account}/${name}/querycache`;
-        fetch(url.toString(), {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ query }),
-        })
-          .then((response) => {
-            if (response.status !== 200) {
-              abortController.abort();
-            } else {
-              url.pathname = `/${account}/${name}.json`;
-              url.searchParams.set("query", query);
-              fetch(url.toString())
-                .then((r) => r.json())
-                .then((data) => resolve({ data }));
-            }
-          })
-          .catch(() => abortController.abort());
-      } else {
-        url.pathname = `/${account}/${name}.json`;
-        fetch(url.toString())
-          .then((r) => r.json())
-          .then((data) => resolve({ data }));
-      }
+      requestUrl = new URL(`/${account}/${name}.json`, config.dataconnectorHost);
+      maptoolkitUrl.searchParams.forEach((value, key) => {
+        requestUrl?.searchParams.set(key, value);
+      });
     }
     if (requestUrl) {
       requestUrl.searchParams.set("api_key", config.apiKey);
